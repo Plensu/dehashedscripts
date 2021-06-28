0009e2c7e9bdccd1f6d05b63f41137ae7ebd91d1
schumact <carsten_schumaker@yahoo.com>
2021-06-28 16:40:00 -0500

    shortened code by getting rid of conditionals

--- dehashedapi.py
+++ dehashedapi.py
@@ -28,126 +28,18 @@ parser.add_argument('-o', '--output', help='Output the output to a file.')
 
 args = parser.parse_args()
 endpoint = "https://api.dehashed.com/search?query="
-i = 0
-if args.domain:
-    if i < 1:
-        endpoint = endpoint + "domain:" + args.domain
-        i += 1
-    else:
-        endpoint = endpoint + "&domain:" + args.domain
-        i += 1
+params = ["domain", "id", "email", "username", "ip_address", "password", "hashed_pasword",
+          "hash_type", "name", "vin", "phone", "address", "database_name", "size", "page"]
 
-if args.id:
-    if i < 1:
-        endpoint = endpoint + "id:" + args.id
-        i += 1
-    else:
-        endpoint = endpoint + "&id:" + args.id
-        i += 1
+is_param_added = False
+for k, v in args.__dict__.items():
+    if v:
+        if is_param_added:
+            endpoint += "&{0}:{1}".format(k, v)
+        else:
+            endpoint += "{0}:{1}".format(k, v)
+            is_param_added = True
 
-if args.email:
-    if i < 1:
-        endpoint = endpoint + "email:" + args.email
-        i += 1
-    else:
-        endpoint = endpoint + "&email:" + args.email
-        i += 1
-
-if args.ipaddr:
-    if i < 1:
-        endpoint = endpoint + "ip_address:" + args.ipaddr
-        i += 1
-    else:
-        endpoint = endpoint + "&ip_address:" + args.ipaddr
-        i += 1
-
-if args.username:
-    if i < 1:
-        endpoint = endpoint + "username:" + args.username
-        i += 1
-    else:
-        endpoint = endpoint + "&username:" + args.username
-        i += 1
-
-if args.passwd:
-    if i < 1:
-        endpoint = endpoint + "password:" + args.passwd
-        i += 1
-    else:
-        endpoint = endpoint + "&password:" + args.passwd
-        i += 1
-
-if args.hashedpasswd:
-    if i < 1:
-        endpoint = endpoint + "hashed_password:" + args.hashedpasswd
-        i += 1
-    else:
-        endpoint = endpoint + "&hashed_password:" + args.hashedpasswd
-        i += 1
-
-if args.hashtype:
-    if i < 1:
-        endpoint = endpoint + "hash_type:" + args.hashtype
-        i += 1
-    else:
-        endpoint = endpoint + "&hash_type:" + args.hashtype
-        i += 1
-
-if args.name:
-    if i < 1:
-        endpoint = endpoint + "name:" + args.name
-        i += 1
-    else:
-        endpoint = endpoint + "&name:" + args.name
-        i += 1
-
-if args.vin:
-    if i < 1:
-        endpoint = endpoint + "vin:" + args.vin
-        i += 1
-    else:
-        endpoint = endpoint + "&vin:" + args.vin
-        i += 1
-
-if args.address:
-    if i < 1:
-        endpoint = endpoint + "address:" + args.address
-        i += 1
-    else:
-        endpoint = endpoint + "&address:" + args.address
-        i += 1
-
-if args.phone:
-    if i < 1:
-        endpoint = endpoint + "phone:" + args.phone
-        i += 1
-    else:
-        endpoint = endpoint + "&phone:" + args.phone
-        i += 1
-
-if args.database:
-    if i < 1:
-        endpoint = endpoint + "database_name:" + args.database
-        i += 1
-    else:
-        endpoint = endpoint + "&database_name:" + args.database
-        i += 1
-
-if args.size:
-    if i < 1:
-        endpoint = endpoint + "size:" + args.size
-        i += 1
-    else:
-        endpoint = endpoint + "&size:" + args.size
-        i += 1
-
-if args.page:
-    if i < 1:
-        endpoint = endpoint + "page:" + args.page
-        i += 1
-    else:
-        endpoint = endpoint + "&page:" + args.page
-        i += 1
 if args.output:
     print("The request sent is: " + endpoint)
     response = requests.get(endpoint, headers=headers, auth=(dhusername, dhapikey))
@@ -160,3 +52,4 @@ else:
     response = requests.get(endpoint, headers=headers, auth=(dhusername, dhapikey))
     print(response.status_code)
     print(response.json())
+
